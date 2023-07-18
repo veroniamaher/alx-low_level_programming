025e940682230040aa6dd8b79ac20a467d5a2920
veroniamaher <veroniamaher2@gmail.com>
2023-07-18 16:36:49 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - Check for alphabetic order
+ *
+ * @c: alphabet to check for
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
